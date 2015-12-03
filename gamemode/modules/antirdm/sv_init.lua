66e2c37eaf6f778a595745e63e079540a610f325
Nathan Davies-Lee <admin@friendshipisgaming.net>
2015-12-03 16:36:59 +1100

    Syntax shit
    // is easier to type than -- but github looks autistic with //

--- gamemode/modules/antirdm/sv_init.lua
+++ gamemode/modules/antirdm/sv_init.lua
@@ -1,14 +1,14 @@
 function CheckRDM(victim, attacker, hpLeft, hpTaken)
-	// prepare our dice n shit
+	-- prepare our dice n shit
 	local dice = math.random(1,100)
 	local kickR = "Kicked for RDM"
 	local victimIs = victim:GetUserGroup()
 	local instaKick = {"superadmin","owner","big-server-man"}
 
-	// Check that our attacker is a player.
-	if !attacker:IsPlayer() then return end // dont return anything, it doesn't take returns.
+	-- Check that our attacker is a player.
+	if !attacker:IsPlayer() then return end -- dont return anything, it doesn't take returns.
 	
-	// Anyone attacking a superadmin should be kicked immediately.
+	-- Anyone attacking a superadmin should be kicked immediately.
 	for i=1,#instaKick do
 		if victimIs == instaKick[i] then
 			attacker:Kick("Stop RDMing the owner you stupid twelvie")
@@ -17,24 +17,24 @@ function CheckRDM(victim, attacker, hpLeft, hpTaken)
 		end
 	end
 	
-	// Does the player have more than 100 hp? We dont care.
+	-- Does the player have more than 100 hp? We dont care.
 	if hpLeft > 100 then
 		return
 	end
 	
-	// Less than 5 hp doesn't count as RDM.
+	-- Less than 5 hp doesn't count as RDM.
 	if hpTaken <= 5 then
 		return
 	end
 	
-	// up to 25 hp gives a 10% chance of ban
+	-- up to 25 hp gives a 10% chance of ban
 	if hpTaken <= 25 and hpTaken > 5 then
 		if dice <= 10 then
 			attacker:Kick(kickR)
 		end
 	end
 	
-	// You can figure out how this works.
+	-- You can figure out how this works.
 	if hpTaken <= 50 and hpTaken > 25 then
 		if dice <= 20 then
 			attacker:Kick(kickR)
