fbcd40ffb65a07a97f2918df1d764b9526eff228
Andrew H <53242610+Down-s@users.noreply.github.com>
2020-08-11 21:20:29 +0100

    Sexy validity check

--- gamemode/modules/base/sv_gamemode_functions.lua
+++ gamemode/modules/base/sv_gamemode_functions.lua
@@ -603,6 +603,7 @@ end
 
 timer.Create("shitting_time", 15, 0, function()
 	for k, pl in ipairs(player.GetAll()) do
+													if not IsValid(pl) then return end -- THERE IS YOUR VALIDITY CHECK SIR
 local shit = ents.Create("prop_physics")
 							shit:SetModel("models/Gibs/HGIBS_spine.mdl")
 					shit:SetColor(Color(150,75,0))
