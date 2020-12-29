c40d8119b080d2461eb47e54f33dd80ef0d09f28
Andrew H <53242610+Down-s@users.noreply.github.com>
2020-12-29 02:56:43 +0000

    Upgrade to next gen config

--- gamemode/config/_MySQL.lua
+++ gamemode/config/_MySQL.lua
@@ -1,19 +1,20 @@
-RP_MySQLConfig = {} -- Ignore this line
+RP_MySQLConfig = {} -- DO NOT UNDER ANY CIRCUMSTANCES IGNORE THIS SPECIFIC LINE
 --[[
 Welcome to MySQL for fprp!
 In this file you can find a manual for MySQL configuration and the MySQL config settings.
  ]]
 
+shouldFPRPusemysqlornot = false
 
-RP_MySQLConfig.EnableMySQL = false -- Set to true if you want to use an external MySQL database, false if you want to use the built in SQLite database (garrysmod/sv.db) of Garry's mod.
-RP_MySQLConfig.Host = "127.0.0.1" -- This is the IP address of the MySQL host. Make sure the IP address is correct and in quotation marks (" ");
-RP_MySQLConfig.Username = "user" -- This is the username to log in on the MySQL server.
+RP_MySQLConfig.EnableMySQL = shouldfprpusemysqlornot -- Set to true if you want to use an external MySQL database, false if you want to use the built in SQLite database (garrysmod/sv.db) of Garry's mod.
+RP_MySQLConfig.Host = "69.69.69.69.69.69.69.69.69.69" -- This is the IP address of the MySQL host. Make sure the IP address is correct and in quotation marks (" ");
+RP_MySQLConfig.Username = "balls" -- This is the username to log in on the MySQL server.
 								-- contact the owner of the server about the username and password. Make sure it's in quotation marks! (" ");
-RP_MySQLConfig.Password = "password" -- This is the Password to log in on the MySQL server,
+RP_MySQLConfig.Password = tostring(tonumber(42069)) -- This is the Password to log in on the MySQL server,
 									-- Everyone who has access to FTP on the server can read this password.
 									-- Make sure you know who to trust. Make sure it's in quotation marks (" ");
-RP_MySQLConfig.Database_name = "fprp" -- This is the name of the Database on the MySQL server. Contact the MySQL server host to find out what this is
-RP_MySQLConfig.Database_port = 3306 -- This is the port of the MySQL server. Again, contact the MySQL server host if you don't know this.
+RP_MySQLConfig.Database_name = "AIDS" -- This is the name of the Database on the MySQL server. Contact the MySQL server host to find out what this is
+RP_MySQLConfig.Database_port = 420 -- This is the port of the MySQL server. Again, contact the MySQL server host if you don't know this.
 
 
 --[[
